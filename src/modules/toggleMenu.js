4d42592f0bc8254af31ad5037d7f60c160eeef99
SaltyBr <SaltyBrain@yandex.ru>
2020-11-26 11:17:04 +0300

    Добавил функционал меню и модальным окнам

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,7 +1,51 @@
 'use strict';
 
 const toggleMenu = ()=>{
-  console.log('hello world');
+  const listId = document.getElementById('list-id'),
+        freeVisitForm = document.getElementById('free_visit_form'),
+        callbackForm = document.getElementById('callback_form');
+
+  // функция открывания/закрывания меню при клике по кнопке
+  const handlerMenu = (item) =>{
+    if(item.style.display === 'block'){
+      item.style.display = 'none';
+    } else{
+      item.style.display = 'block';
+    }
+  };
+
+  // функция закрытия модального окна на крестик или если клик был вне модального окна, принимает id формы
+  const closeModal = (formId)=>{
+    document.getElementById(`${formId}`).querySelector('.close_icon').addEventListener('click', ()=>{
+      document.getElementById(`${formId}`).style.display = 'none';
+    });
+    document.getElementById(`${formId}`).addEventListener('click', (event)=>{
+      let target = event.target;
+      if(!target.closest(`.form-wrapper`)){
+        document.getElementById(`${formId}`).style.display = 'none';
+      }
+    });
+  };
+
+  closeModal('free_visit_form');
+  closeModal('callback_form');
+
+
+  document.addEventListener('click', (event)=>{
+    let target = event.target;
+
+    if(target.closest('.clubs-list')){
+      handlerMenu(listId);
+    }
+    if(target.closest('.free-visit')){
+      handlerMenu(freeVisitForm);
+    }
+    if(target.matches('.callback-btn')){
+      handlerMenu(callbackForm);
+    }
+  });
+
+  
 };
 
 export default toggleMenu;
