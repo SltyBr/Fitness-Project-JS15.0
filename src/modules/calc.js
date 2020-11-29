870d3daf1228047bccf6caf41232d0a63abbc0f1
SaltyBr <SaltyBrain@yandex.ru>
2020-11-29 23:04:53 +0300

    сделал калькулятор

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,68 +1,62 @@
 const calc = ()=>{
-/*   const firstCase = document.getElementById('card_leto_mozaika'),
-        secondCase = document.getElementById('card_leto_schelkovo'),
+  const formCard = document.getElementById('card_order'),
         months = document.querySelector('.time'),
+        cardFirstCase = document.getElementById('card_leto_mozaika'),
+        cardSecondCase = document.getElementById('card_leto_schelkovo'),
         priceTotal = document.getElementById('price-total'),
-        promocode = document.getElementById('promocode'),
-        monthsOptions = months.querySelectorAll('input[type="radio"]');
+        monthsOptions = months.querySelectorAll('input[type="radio"]');     
 
   const pricesFirstCase = [1999, 9900, 13900, 19900];
   const pricesSecondCase = [2999, 14900, 21900, 24900];
   const prices = {};
+  let result = 0;
 
-  const getPriceMonth = (option, price)=>{
-    monthsOptions.forEach((item, index)=>{
-        if(option.checked && item.checked){
-          prices[item.value] = price[index];
-          for(let key in prices){
-            if(item.value == key){
-              priceTotal.textContent = `${prices[key]} р.`;
-              promocode.addEventListener('change', ()=>{
-                if(promocode.value === 'ТЕЛО'){
-                  let newPrice = prices[key]*0.7;
-                  promocode.style.border = '3px solid green';
-                  priceTotal.textContent = `${newPrice} р.`;
-                } else{
-                  alert('неверный промокод, попробуйте ещё раз');
-                  promocode.value = '';
-                }
-              });
-            }
+  const priceCase = ()=>{
+    if(cardFirstCase.checked){
+      monthsOptions.forEach((item, index)=>{
+        prices[item.value] = pricesFirstCase[index];
+      });
+    } else if(cardSecondCase.checked){
+      monthsOptions.forEach((item, index)=>{
+        prices[item.value] = pricesSecondCase[index];
+      });
+    }
+  };
+  priceCase();
+
+  const priceOptions = ()=>{
+    monthsOptions.forEach((item)=>{
+      if(item.checked){
+        for(let key in prices){
+          if(key == item.value){
+            result = prices[key];
+            priceTotal.textContent = result;
           }
         }
-
+      }
     });
   };
+  priceOptions();
 
-  const getPriceCheck = (option, price)=>{
-    option.addEventListener('click', ()=>{
-      monthsOptions.forEach((item, index)=>{
-        prices[item.value] = price[index];
-        item.addEventListener('click', ()=>{
-          for(let key in prices){
-            if(item.value == key){
-              priceTotal.textContent = `${prices[key].toFixed(0)} р.`;
-              promocode.addEventListener('change', ()=>{
-                if(promocode.value === 'ТЕЛО'){
-                  let newPrice = prices[key]*0.7;
-                  promocode.style.border = '3px solid green';
-                  priceTotal.textContent = `${newPrice.toFixed(0)} р.`;
-                } else{
-                  alert('неверный промокод, попробуйте ещё раз');
-                  promocode.value = '';
-                }
-              });
-            }
-          }
-        });
-      });
-    });
+  const promoPrice = ()=>{
+    if(formCard.querySelector('#promocode').value === 'ТЕЛО2020'){
+      result = Math.ceil(result*0.7);
+      priceTotal.textContent = result;
+    }
   };
 
-  getPriceCheck(firstCase, pricesFirstCase);
-  getPriceMonth(firstCase, pricesFirstCase);
-  getPriceCheck(secondCase, pricesSecondCase);
-  getPriceMonth(secondCase, pricesSecondCase); */
+  formCard.addEventListener('change', (event)=>{
+    let target = event.target;
+    if(target.matches('input[type="radio"]')){
+      priceCase();
+      priceOptions();
+      promoPrice();
+    }
+    if(target.matches('#promocode')){
+      promoPrice();
+    }
+  });
+  
 };
 
 export default calc;
