03b81abb16ce34984bf439f534cb0ea9008e860c
SaltyBr <SaltyBrain@yandex.ru>
2020-11-29 12:57:38 +0300

    полностью сделал отправку формы визита, пересобрал проект

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,7 +1,33 @@
 const sendForm = (formId)=>{
-  const form = document.getElementById(`${formId}`);
+  const form = document.getElementById(`${formId}`),
+        thanksMessage = document.getElementById('thanks');
+
+  const messageContent = (content)=>{  // функция создания сообщения о статусе заявки
+    let successMessage = document.createElement('div');
+      successMessage.style.cssText = 'font-size: 1rem; color: red';
+      successMessage.textContent = content;
+    return successMessage;
+  };
+
+  const alertMessage = messageContent('Заполните все поля');
+  const errorMessage = messageContent('Что-то пошло не так, попробуйте ещё раз');
+
+
+
+  const removeElement = (el)=>{
+    setTimeout(function(){
+      el.remove();
+    }, 2000);
+  };
+
+
 
   let body = {};
+
+  const personalData = form.querySelector('.personal-data');
+
+  personalData.querySelector('input[type="checkbox"]').classList.add('_req');
+  personalData.querySelector('label').classList.add('checkbox__label');
 
   form.addEventListener('submit', formSend);
 
@@ -22,12 +48,16 @@ const sendForm = (formId)=>{
         body: JSON.stringify(body)
       });
       if(response.ok){
+        thanksMessage.style.display = 'block';
+        removeElement(thanksMessage);
         form.reset();
       } else{
-        alert('Ошибка');
+        form.append(errorMessage);
+        removeElement(errorMessage);
       }
     } else{
-      alert('Дайте согласие на обработку');
+      form.append(alertMessage);
+      removeElement(alertMessage);
     }
   }
 
